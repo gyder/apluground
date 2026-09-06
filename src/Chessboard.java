72de9f56edfbc1c63145d5038a12f55f88cc7bdb
Paul Stein <paul@caladon.de>
2026-09-06 11:13:42 +0200

    Initial Chessboard

---
+++ src/Chessboard.java
@@ -0,0 +1,15 @@
+import ch.aplu.util.*;
+
+class Chessboard extends GPanel {
+    Chessboard() {
+        window(0, 8, 0, 8);
+        for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 8; j++)
+                if ((i + j) % 2 == 0)
+                    fillRectangle(i, j, i + 1, j + 1);
+    }
+
+    static void main(String[] args) {
+        new Chessboard();
+    }
+}
